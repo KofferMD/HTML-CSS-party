47e7f4f0743332ab8ece31f08875bebe9f0e6658
Nikolay <n_vinok98@mail.ru>
2021-03-08 18:12:01 +0300

    mediaAdded

--- js/main.js
+++ js/main.js
@@ -3,7 +3,15 @@ $(function(){
         arrows: false,
         dots: true,
         autoplay: true,
-        fade: true
+        fade: true,
+        responsive: [
+            {
+              breakpoint: 1200,
+              settings: {
+                dots: false
+              }
+            },
+          ]
     });
 
     $('.reviews__slider').slick({
@@ -11,6 +19,35 @@ $(function(){
         dots: true,
         slidesToShow: 4,
         slidesToScroll: 1,
+        autoplay: true,
+        responsive: [
+            {
+              breakpoint: 1140,
+              settings: {
+                slidesToShow: 3,
+                dots: true
+              }
+            },
+            {
+                breakpoint: 846,
+                settings: {
+                  slidesToShow: 2,
+                  dots: true
+                }
+              },
+              {
+                breakpoint: 585,
+                settings: {
+                  slidesToShow: 1,
+                  dots: true
+                }
+              },
+          ]
     });
+
+    $('.menu__btn').on('click', function (){
+        $('.menu__list').toggleClass('menu__list--active');
+    });
+  
 
 });
